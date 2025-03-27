8c2b424ea46288e3089b084c5e2bf0a8bb3c79a7
Michael Chrupcala <michael@artblocks.io>
2025-03-27 18:51:41 -0400

    changes from the tech interview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,33 @@ interface CalendarEvent extends RBCEvent {
   end: Date;
 }
 
+interface User {
+  id: string;
+  createdAt: string;
+  username: string;
+  password: string;
+}
+
 export default function Home() {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+  const [isCreatingNewEvent, setIsCreatingNewEvent] = useState(false);
+  const [eventName, setEventName] = useState("");
+  const [userName, setUserName] = useState("");
+
+  function extractUsernames(users) {
+    return users.map((user) => user.username);
+  }
+
+  function handleEventName(e) {
+    console.log(e.target.value);
+    setEventName(e.target.value);
+  }
+
+  function handleUsername(e) {
+    console.log(e.target.value);
+    setUserName(e.target.value);
+  }
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -37,17 +62,39 @@ export default function Home() {
         start: e.start,
         end: e.end,
       }));
-      console.log(mapped);
+
       setEvents(mapped);
     };
 
     fetchEvents();
   }, []);
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await fetch("/api/users");
+      const data = await res.json();
+      const mapped = data.map((e: any) => ({
+        id: e.id,
+        createdAt: e.createdAt,
+        username: e.username,
+        end: e.end,
+      }));
+
+      setUsers(mapped);
+    };
+
+    fetchUsers();
+    console.log(users);
+  }, []);
+
   const handleSlotSelect = useCallback(async ({ start, end, action }) => {
+    console.log(users);
+
     if (action === "doubleClick") {
-      const title = window.prompt("New Event name");
-      if (!title) return;
+      setIsCreatingNewEvent(true);
+      // const title = window.prompt("New Event name");
+      const title = eventName;
+      if (!title || !userName) return;
 
       const body = {
         name: title,
@@ -134,12 +181,24 @@ export default function Home() {
     setEvents((prev) => prev.filter((e) => e.id !== id));
   }, []);
 
-  const handleSelectEvent = useCallback((event) => {
-    window.alert(event.title);
-  }, []);
+  // const handleSelectEvent = useCallback((event) => {
+  //   window.alert(event.title);
+  // }, []);
 
   return (
     <div className="App">
+      {isCreatingNewEvent && (
+        <div className="z-50 bg-amber-900 h-100 w-100 absolute top-30 left-30">
+          <input id="eventName" onChange={(e) => handleEventName(e)} />
+          <select onChange={(e) => handleUsername(e)}>
+            {extractUsernames(users).map((username, idx) => {
+              return <option key={idx}>{username}</option>;
+            })}
+          </select>
+          <button onClick={() => console.log("clicked")}></button>
+        </div>
+      )}
+
       <DnDCalendar
         selectable
         defaultDate={moment().toDate()}
@@ -148,10 +207,10 @@ export default function Home() {
         localizer={localizer}
         onEventDrop={onEventDrop}
         onEventResize={onEventResize}
-        onSelectEvent={handleSelectEvent}
+        onSelectEvent={(e) => console.log(e.title)}
         onSelectSlot={handleSlotSelect}
         resizable
-        style={{ height: "100vh" }}
+        style={{ height: "100vh", zIndex: "0" }}
         components={{
           event: (props) => (
             <CancelEventIcon {...props} onDelete={handleDeleteEvent} />
